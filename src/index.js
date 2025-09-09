6c4ab4fc0dda43071944510b8c95bf10de0b511e
YuliaCoolinich <64796608+YuliaCoolinich@users.noreply.github.com>
2025-09-09 19:34:34 +0300

    Added footer links as details component

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import './styles/components/swiper.css';
 import './styles/components/featured-collection.css';
 import './styles/components/faq.css';
 import './styles/components/help.css';
+import './styles/components/footer.css';
 
 const helpForm = document.getElementById('help-form');
 helpForm.addEventListener('submit', (e) => {
@@ -17,3 +18,58 @@ helpForm.addEventListener('submit', (e) => {
   alert('Thank you!');
   helpForm.reset();
 });
+
+const generateFooterDetails = () => {
+  const isMobileWidth = window.innerWidth < 768;
+
+  if (isMobileWidth) {
+    console.log('here');
+    const footerItems = [
+      {
+        datasetContainer: document.getElementById('footer-list-item-1'),
+        summary:
+          document.getElementById('footer-list-item-1')?.children[0]
+            ?.textContent,
+        content: document.getElementById('footer-list-item-1')?.children[1],
+      },
+      {
+        datasetContainer: document.getElementById('footer-list-item-2'),
+        summary:
+          document.getElementById('footer-list-item-2')?.children[0]
+            ?.textContent,
+        content: document.getElementById('footer-list-item-2')?.children[1],
+      },
+      {
+        datasetContainer: document.getElementById('footer-list-item-3'),
+        summary:
+          document.getElementById('footer-list-item-3')?.children[0]
+            ?.textContent,
+        content: document.getElementById('footer-list-item-3')?.children[1],
+      },
+      {
+        datasetContainer: document.getElementById('footer-list-item-4'),
+        summary:
+          document.getElementById('footer-list-item-4')?.children[0]
+            ?.textContent,
+        content: document.getElementById('footer-list-item-4')?.children[1],
+      },
+    ];
+    footerItems.forEach((item) => {
+      const details = document.createElement('details');
+      const summary = document.createElement('summary');
+
+      summary.textContent = item.summary;
+
+      details.appendChild(summary);
+      details.appendChild(item?.content);
+      details.className = 'footer-section-container';
+      item.datasetContainer.innerHTML = '';
+      item.datasetContainer.appendChild(details);
+    });
+  } else {
+    //to-do add generation
+  }
+};
+
+// window.addEventListener('resize', generateFooterDetails);
+// window.addEventListener('load', generateFooterDetails);
