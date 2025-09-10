b17e3bd4e16ecae4676f2d39eec7124fe0f0946e
YuliaCoolinich <64796608+YuliaCoolinich@users.noreply.github.com>
2025-09-11 02:07:59 +0300

    Updated location of index.html

--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html',
+      template: 'index.html',
       filename: 'index.html',
     }),
     new CopyPlugin({
